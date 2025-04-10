80118e5cfa9911b5edee37114e0361ca9d6a065b
erikavillamin <erikamaevillamin@gmail.com>
2025-04-10 17:55:26 +0800

    This is a while loop that will allow user to input many questions and the choices until they choose to exit.

---
+++ quiz_creator.py
@@ -0,0 +1,8 @@
+# This is a program that creates quizzes
+while True:
+    quiz_question = input("Enter a question: ") #Ask the user the questions they want to input
+    # These are the choices or possible answers to the question
+    letter_a = input("Enter letter a: ")
+    letter_b = input("Enter letter b: ")
+    letter_c = input("Enter letter c: ")
+    letter_d = input("Enter letter d: ")
