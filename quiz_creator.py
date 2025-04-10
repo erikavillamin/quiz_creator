a8760c02d44562927f93e10f679b894a894adb32
erikavillamin <erikamaevillamin@gmail.com>
2025-04-10 18:09:20 +0800

    This is a nested loop that validate the correct letter of answer.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,16 @@
 # This is a program that creates quizzes
-while True:
+while True: # A loop that allow user to input many questions 
     quiz_question = input("Enter a question: ") #Ask the user the questions they want to input
     # These are the choices or possible answers to the question
     letter_a = input("Enter letter a: ")
     letter_b = input("Enter letter b: ")
     letter_c = input("Enter letter c: ")
     letter_d = input("Enter letter d: ")
+
+    while True: # This is a nested loop to validate correct answer
+        correct_ans = input("What letter is the correct answer? ")
+        if correct_ans in ['a', 'b', 'c', 'd']:
+            break  # Exit loop
+        else:
+            print("Invalid.") # The input shpould be a, b, c, d only
+
