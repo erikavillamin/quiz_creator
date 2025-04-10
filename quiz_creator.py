fbb7f6e6c45baed4b25c286e38a521d6df3a14dc
erikavillamin <erikamaevillamin@gmail.com>
2025-04-10 22:24:37 +0800

    add emojis

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,22 +1,24 @@
 # This is a program that creates quizzes
+import emoji
 with open("quiz_creator_questions.txt", "a") as file: # This will open the file and add the users input without deleting the old content
     
     while True: # A loop that allow user to input many questions 
-        quiz_question = input("Enter a question: ") #Ask the user the questions they want to input
+        quiz_question = input(emoji.emojize("Enter a question:thinking_face:: ")) #Ask the user the questions they want to input
         # These are the choices or possible answers to the question
-        letter_a = input("Enter letter a: ")
-        letter_b = input("Enter letter b: ")
-        letter_c = input("Enter letter c: ")
-        letter_d = input("Enter letter d: ")
+        letter_a = input(emoji.emojize("Enter letter a :red_apple:: "))
+        letter_b = input(emoji.emojize("Enter letter b :boy:: "))
+        letter_c = input(emoji.emojize("Enter letter c :cow:: "))
+        letter_d = input(emoji.emojize("Enter letter d :dog:: "))
         
         while True: # This is a nested loop to validate correct answer
-            correct_ans = input("What letter is the correct answer? ")
+            correct_ans = input(emoji.emojize("What letter is the correct answer?:thinking_face: "))
             if correct_ans in ['a', 'b', 'c', 'd']:
                 break  # Exit loop
             else:
-                print("Invalid.") # The input shoould be a, b, c, d only
+                print(emoji.emojize("Invalid.:police_car_light: ")) # The input shoould be a, b, c, d only
         
-        file_save = input("Do you want to save this question? (yes/no): ").strip().lower()  # Ask user if they want to save their input
+
+        file_save = input(f"Do you want to save this question? (yes/no): ").strip().lower()
         if file_save in ["yes", "y"]:
             file.write(quiz_question + "\n")     # It will write the quiz question to a file         
             file.write("a) " + letter_a + "\n")
@@ -25,13 +27,13 @@ with open("quiz_creator_questions.txt", "a") as file: # This will open the file 
             file.write("d) " + letter_d + "\n")
             file.write(correct_ans + "\n")
             file.write("\n")
-            print("Question saved!\n")
+            print(emoji.emojize("Question saved!:grinning_face_with_smiling_eyes:\n"))
         else:
-            print("Question not saved.\n")
+            print(emoji.emojize("Question not saved.:frowning_face:\n"))
                 
         another_ques = input("Do you want to add another question? (yes/no): ").strip().lower()   # Ask user if they want to add another questions
         if another_ques in ["no", "n"]:
-            print("Thank you for using Quiz Creator.")  # Exiting the program
+            print(emoji.emojize("Thank you for using Quiz Creator.:waving_hand:"))  # Exiting the program
             break
 
 
